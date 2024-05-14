d9d5d5824b870e8d502b36e6ec580e63feccafac
Caio Marx <icaiomarxm@gmail.com>
2024-05-13 21:43:51 -0300

    feat: forecast CLI

--- main.go
+++ main.go
@@ -1,21 +1,106 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
+	"time"
+
+	"github.com/fatih/color"
 )
 
-func main() {
-	name := flag.String("name", "world", "The name to greet.")
-	flag.Parse()
-	url := buildUrl()
-	fmt.Printf("Hello, %s!\n", *name)
-	fmt.Printf("Hello, %s!\n", url)
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch int64   `json:"time_epoch"`
+				TempC     float64 `json:"temp_c"`
+				Condition struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
 }
 
-func buildUrl() string {
+func main() {
+	city := flag.String("city", "Belo Horizonte", "The city to check the weather.")
+	flag.Parse()
+
+	apiUrl := buildUrl(*city)
+	res, err := http.Get(apiUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic("Weather API not available")
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("%s, %s: %.1fC, %s\n",
+		location.Name,
+		location.Country,
+		current.TempC,
+		current.Condition.Text,
+	)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+
+		if date.Before(time.Now()) {
+			continue
+		}
+
+		message := fmt.Sprintf("%s - %.2fC, %.0f%%, %s\n",
+			date.Format("15:04"),
+			hour.TempC,
+			hour.ChanceOfRain,
+			hour.Condition.Text,
+		)
+
+		if hour.ChanceOfRain < 40 {
+			fmt.Print(message)
+		} else {
+			color.Red(message)
+		}
+	}
+}
+
+func buildUrl(city string) string {
 	base_url := os.Getenv("BASE_URL")
 	api_key := os.Getenv("API_KEY")
-	return base_url + "current.json?key=" + api_key
+	return base_url + "/forecast.json" + "?key=" + api_key + "&q=" + url.QueryEscape(city)
 }
